8c109c3542dfaa607457e1304b2d69183d7e32ab
Mohammed Gulam Rabbi <41266775+rabbi-630@users.noreply.github.com>
2025-10-03 01:24:23 +0600

    u.js

--- u.js
+++ u.js
@@ -2,4 +2,4 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&amp;c='+Math.random();f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TLQ8HF4X');
+})(window,document,'script','dataLayer','GTM-TZK8PKQP');
